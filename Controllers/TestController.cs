23fcb8c47ccc836c23e6fd84440c03294a9ab7a7
AntimonyIQ <antimonyiq@gmail.com>
2022-05-26 10:29:16 +0100

    published to azure

---
+++ Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace sybatnetwork.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            //Fetch the JSON string from URL.
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            string json = (new WebClient()).DownloadString("https://raw.githubusercontent.com/aspsnippets/test/master/Customers.json");
+            // var x = JsonConvert.SerializeObject(json);
+            dynamic parsedJson = JsonConvert.DeserializeObject(json);
+ 
+            //Return the JSON string.
+            return Content(JsonConvert.SerializeObject(parsedJson, Formatting.Indented));
+        }
+    }
+}
