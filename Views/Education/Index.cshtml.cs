5fbe74ccaa3e33c05ba6f945a6372cc2f4920605
AntimonyIQ <antimonyiq@gmail.com>
2022-07-25 11:38:56 +0100

    added education

---
+++ Views/Education/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace sybatnetwork.Views.Education
+{
+    public class IndexModel : PageModel
+    {
+        // private const string webTitle = "Educations";
+        public void OnGet()
+        {
+        }
+    }
+}
