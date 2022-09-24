c7cdfd648e7d6c2d204890a35c6e67927f7a6b87
AntimonyIQ <antimonyiq@gmail.com>
2022-09-24 20:19:42 +0100

    Added sales page

---
+++ Controllers/SalesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sybatnetwork.Controllers
+{
+    public class SalesController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Sales";
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            ViewData["Title"] = "Contact Sales";
+            return View();
+        }
+    }
+}
---
+++ Views/Sales/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace sybatnetwork.Views.Sales
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
