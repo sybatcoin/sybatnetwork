d6973a470a48b95431b845c186c257e587866bad
AntimonyIQ <antimonyiq@gmail.com>
2022-09-25 12:40:30 +0100

    added entity database technology to the app

---
+++ Models/Sales.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+using System.Web;
+
+namespace sybatnetwork.Models
+{
+    public class Sales
+    {
+        public int ID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Organisation { get; set; }
+        public string OrganisationType { get; set; }
+        public string Country { get; set; }
+        public string Message { get; set; }
+        public DateTime JoiningDate { get; set; }
+    }
+    public class EmpDBContext : DbContext
+    {
+        public EmpDBContext()
+        { }
+        public DbSet<Sales> sales { get; set; }
+    }
+}
