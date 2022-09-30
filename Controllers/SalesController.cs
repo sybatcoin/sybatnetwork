74d34a2bb023762d96f974225a108aa027a4a030
AntimonyIQ <antimonyiq@gmail.com>
2022-09-30 02:24:26 +0100

    form construction

--- Controllers/SalesController.cs
+++ Controllers/SalesController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using sybatnetwork.Models;
 
 namespace sybatnetwork.Controllers
 {
@@ -13,10 +15,30 @@ namespace sybatnetwork.Controllers
             ViewData["Title"] = "Sales";
             return View();
         }
+        // /Sales/Contact
+        [HttpGet]
         public IActionResult Contact()
         {
             ViewData["Title"] = "Contact Sales";
+
             return View();
+        }
+        [HttpPost]
+        public IActionResult Contact(Sales model)
+        {
+            ViewData["Title"] = "Contact Sales";
+            var FirstName = model.Firstname;
+            var LastName = model.Lastname;
+            var Email = model.Email;
+            var Organisation = model.Organisation;
+            var OrganisationType = model.OrganisationType;
+            var Country = model.Country;
+            var Message = model.Message;
+
+            ViewData["Title"] = FirstName;
+
+            // return Json(model);
+            return View(model);
         }
     }
 }
