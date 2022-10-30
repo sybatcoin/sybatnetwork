30ea428c77c31cc1bca508082b7c0b769fcd520d
AntimonyIQ <antimonyiq@gmail.com>
2022-10-30 01:11:29 +0100

    added docs and document

---
+++ Controllers/MediaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sybatnetwork.Controllers
+{
+    public class MediaController : Controller
+    {
+        private const string webTitle = "Media";
+        public IActionResult Index()
+        {
+            ViewData["Title"] = webTitle;
+            return View();
+        }
+    }
+}
