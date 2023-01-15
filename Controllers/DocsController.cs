5f5a288bf6b71ed9b9418e1f76f7166fca5b79df
AntimonyIQ <antimonyiq@gmail.com>
2023-01-16 00:20:59 +0100

    current updates

---
+++ Controllers/DocsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sybatnetwork.Controllers
+{
+    public class DocsController : Controller
+    {
+        private const string WebTitle = "Docs";
+        public IActionResult Index()
+        {
+            ViewData["Title"] = WebTitle;
+            return View();
+        }
+    }
+}
---
+++ Views/Docs/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace sybatnetwork.Views.Docs
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
